52855ef5ab55286152b680ac0e3f0a9f8a509e32
kevin <kevinli8831@gmail.com>
2024-08-24 23:13:08 +0800

    FIRST GIT

---
+++ src/components/Headers.jsx
@@ -0,0 +1,13 @@
+const Headers = () => {
+  return (
+    <div className="flex p-10">
+
+      <div className="px-10">Home</div>
+      <div className="px-10">Work</div>
+      <div className="px-10">Contact Us</div>
+
+      <img src='logo192.png' alt="logo"></img>
+    </div>
+  );
+};
+export default Headers;
